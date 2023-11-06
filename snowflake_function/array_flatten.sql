90ba734d5e170dbf44500dd906d19ed586274a4d
motoy3d <motoy3d@gmail.com>
2023-11-06 11:14:50 +0900

    Snowflakeの公式FunctionとしてARRAY_FLATTENがリリースされたので削除。 ARRAY_FLATTEN was released as an official Snowflake function, so it was deleted.

--- snowflake_function/array_flatten.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION PROD.PUBLIC.ARRAY_FLATTEN(SRC ARRAY)
-RETURNS ARRAY
-LANGUAGE JAVASCRIPT
-AS '
-        let ret=[];
-        SRC.forEach(arr => ret = ret.concat(arr));
-        return ret;
-    ';
